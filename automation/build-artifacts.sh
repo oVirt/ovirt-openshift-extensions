e89ef87cee2dc6c0b3c526d8e903fd4bb5209a4a
Roy Golan <rgolan@redhat.com>
2018-02-19 12:35:14 +0200

    CI: build using containers and push to the registry
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -ex
 
-docker build deployment/ovirt-flexdriver
+make container
+make container-push
 
-#mkdir -p /tmp/go/{src,pkg,bin}
-#export GOPATH=/tmp/go
-#export PATH=${PATH}:${GOPATH}/bin
-
-# make sure latest glide is used
-#curl https://glide.sh/get | sh
-
-#glide cc
-#make deps
-#make build-flex
