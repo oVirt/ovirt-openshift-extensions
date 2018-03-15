96c23e77a4d97919ba365376a98a608f0464e1ec
Roy Golan <rgolan@redhat.com>
2018-03-15 08:43:50 +0200

    link check-merged to build-artifacts till the repo transfer
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,8 +1,1 @@
-#!/bin/bash -ex
-
-mkdir -p /tmp/build/src/github.com/rgolangh
-ln -s $HOME /tmp/build/src/github.com/rgolangh/
-export GOPATH=/tmp/build
-cd /tmp/build/src/github.com/rgolangh/ovirt-flexdriver
-
-make build
+build-artifacts.sh
