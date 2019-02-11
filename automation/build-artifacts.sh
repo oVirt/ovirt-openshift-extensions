0ad819ac5a1a8f216f684417837766c539206197
Roy Golan <rgolan@redhat.com>
2019-02-11 22:59:18 +0200

    Generate code coverage report
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -8,3 +8,7 @@ make push-containers
 make apb_build apb_docker_push
 
 cp containers-artifacts.list $EXPORTED_ARTIFACTS || true
+# get the build outputs
+c=$(docker run --rm -d --entrypoint sleep quay.io/rgolangh/ovirt-flexvolume-driver 5m)
+docker cp $c:/tmp/coverage.html $EXPORTED_ARTIFACTS/coverage.html
+docker stop $c
--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -ex
 
+EXPORTED_ARTIFACTS=exported-artifacts
+mkdir -p $EXPORTED_ARTIFACTS
+
 make build-containers
+
+# get the build outputs
+c=$(docker run --rm -d --entrypoint sleep quay.io/rgolangh/ovirt-flexvolume-driver 5m)
+docker cp $c:/tmp/coverage.html $EXPORTED_ARTIFACTS/coverage.html
+docker stop $c
